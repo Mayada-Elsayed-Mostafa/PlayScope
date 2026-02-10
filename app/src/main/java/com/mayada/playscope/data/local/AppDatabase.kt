5e46159141d04726f49d23adfb847c63c4b7a402
Mayyada Elsayed <mayydaelsayed8@gmail.com>
2026-02-10 11:46:13 +0200

    Bump DB version to 2 after adding GameDetailsEntity

--- app/src/main/java/com/mayada/playscope/data/local/AppDatabase.kt
+++ app/src/main/java/com/mayada/playscope/data/local/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.mayada.playscope.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.mayada.playscope.data.local.dao.GamesDao
 import com.mayada.playscope.data.local.entity.GameDetailsEntity
@@ -12,5 +14,25 @@ import com.mayada.playscope.data.local.entity.GameEntity
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun gamesDao(): GamesDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
