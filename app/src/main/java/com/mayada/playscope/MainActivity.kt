51922f1b48d59a79fec509b441a4fadb167f6908
Mayyada Elsayed <mayydaelsayed8@gmail.com>
2026-02-10 10:36:47 +0200

    chore: update manifest and main activity

--- app/src/main/java/com/mayada/playscope/MainActivity.kt
+++ app/src/main/java/com/mayada/playscope/MainActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import com.mayada.playscope.ui.navigation.GameHubNavGraph
 import com.mayada.playscope.ui.theme.PlayScopeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PlayScopeTheme {
-
+                GameHubNavGraph()
             }
         }
     }
